1698e2ef8fd3eb9c97960061143ee5a0144b47b1
Nelson Valverde La Torre <tivit.nvalverde>
2025-05-02 19:36:15 -0500

    Feature(Alert): Alert design (style 1 and style 2)

--- projects/template-html-nv/assets/js/menu/menu-route.js
+++ projects/template-html-nv/assets/js/menu/menu-route.js
@@ -136,6 +136,14 @@ const menuRoutes = [
           },
         ],
       },
+      {
+        name: "Alerts",
+        icon: null,
+        path: "/projects/template-html-nv/pages/components/alerts.html",
+        active: false,
+        span: 2,
+        children: [],
+      },
     ],
   },
   {
